af3899556d8c6fd6ed602360c3514c7d8b984906
Pablo Puletti <pablo@limio.com>
2020-06-24 10:59:45 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1", // "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "notes-app-uploads-puletti" // "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "eu-west-1", //"YOUR_API_GATEWAY_REGION",
+      URL: "https://v9klonwfk7.execute-api.eu-west-1.amazonaws.com/prod" // "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "eu-west-1", //"YOUR_COGNITO_REGION",
+      USER_POOL_ID: "eu-west-1_D2D17cUnb", // "YOUR_COGNITO_USER_POOL_ID",
+      APP_CLIENT_ID: "5j58l70l3m4h4s2ngr8p8debo9", // "YOUR_COGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "eu-west-1:64cf5b84-09eb-4d3a-9e14-c8306aa9752f" // "YOUR_IDENTITY_POOL_ID"
+    }
+  };
